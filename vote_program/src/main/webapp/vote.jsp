03ea02d7ccf46943ccdf3fdc1a13ab6d2ff63191
김민준 <107795830+Qnd1101@users.noreply.github.com>
2023-10-19 09:09:03 +0900

    Add files via upload

---
+++ vote_program/src/main/webapp/vote.jsp
@@ -0,0 +1,110 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<link rel = "stylesheet" href="css/style.css">
+<script type="text/javascript">
+	function checkValue(){
+		if(!document.data.v_jumin.value) {
+			alert("주민번호가 입력되지 않았습니다.");
+			data.v_jumin.focus();
+			return false;
+		}
+		else if(!document.data.v_name.value) {
+			alert("성명이 입력되지 않았습니다.");
+			data.v_name.focus();
+			return false;
+		}
+		else if (!document.data.m_no.value) {
+			alert("후보번호를 입력하세요.");
+			data.m_no.focus();
+			return false;
+		} 
+		else if (!document.data.v_time.value) {
+			alert("투표시간을 입력하세요.");
+			data.v_time.focus();
+			return false;
+		} 
+		else if (!document.data.v_area.value) {
+			alert("투표장소 입력하세요.");
+			data.v_area.focus();
+			return false;
+		}  
+		else if (!document.data.v_confirm.value) {
+			alert("유권자확인을 입력하세요.");
+			data.v_confirm.focus();
+			return false;
+		}
+		alert("투표하기 정보가 정삭적으로 등록되었습니다!");
+		return true;
+	}
+	
+	function refe() {
+		return location.href='vote.jsp';
+	}
+</script>
+</head>
+<body>
+<header>
+	<jsp:include page = "layout/header.jsp"></jsp:include>
+</header>
+<nav>
+	<jsp:include page = "layout/nav.jsp"></jsp:include>
+</nav>
+<section class = "section">
+	<h2>투표하기</h2>
+	<form action="vote_p.jsp" name="data" method="post" onsubmit="return checkValue()">
+		<table border="1" style="margin-left: auto; margin-right: auto; width : 640px; height : 250px; border-collapse : collapse;">
+			<tr>
+				<th>주민번호</th>
+				<td style="padding : 0 12px;"><input type="text" name = "v_jumin"></input> 예 : 8906153154726</td>
+			</tr>
+			<tr>
+				<th>성명</th>
+				<td style="padding : 0 12px;"><input type="text" name = "v_name"></input></td>
+			</tr>
+			<tr>
+				<th>후보번호</th>
+				<td style="padding : 0 12px;">
+					<select name="m_no">
+					<option value="1">[1] 김후보</option>
+					<option value="2">[2] 이후보</option>
+					<option value="3">[3] 박후보</option>
+					<option value="4">[4] 조후보</option>
+					<option value="5">[5] 최후보</option>
+				</select>
+				</td>
+			</tr>
+			
+			<tr>
+				<th>투표시간</th>
+				<td style="padding : 0 12px;"><input type="text" name = "v_time"></input></td>
+			</tr>
+			<tr>
+				<th>투표장소</th>
+				<td style="padding : 0 12px;"><input type="text" name = "v_area"></input></td>
+			</tr>
+			<tr>
+				<th>유권자 확인</th>
+				<td style="padding : 0 12px;">
+				<input type="radio" name="v_confirm" value="Y"></input>확인
+				<input type="radio" name="v_confirm" value="N"></input>미확인
+				</td>
+			</tr>
+			<tr>
+				<td colspan = "2" style = "text-align : center;">
+					<input type="submit" value="투표하기"></input>
+					<input type="button" value="다시쓰기" onclick="refe()"></input>
+				</td>
+			</tr>
+		</table>
+	</form>
+</section>
+<footer>
+	<jsp:include page = "layout/footer.jsp"></jsp:include>
+</footer>
+</body>
+</html>
---
+++ vote_program/src/main/webapp/지역구.sql
@@ -0,0 +1,80 @@
+create table tbl_vote_202005(
+	v_jumin char(13) primary key,
+	v_name varchar2(20),
+	m_no char(1),
+	v_time char(4),
+	v_area char(20),
+	v_confirm char(1)
+);
+
+insert into tbl_vote_202005 values('99010110001', '김유권', '1', '0930', '제1투표장', 'N');
+
+insert into tbl_vote_202005 values('89010120002', '이유권', '2', '0930', '제1투표장', 'N');
+insert into tbl_vote_202005 values('69010110003', '박유권', '3', '0930', '제1투표장', 'Y');
+insert into tbl_vote_202005 values('59010120004', '홍유권', '4', '0930', '제1투표장', 'Y');
+insert into tbl_vote_202005 values('79010110005', '조유권', '5', '0930', '제1투표장', 'Y');
+insert into tbl_vote_202005 values('89010120006', '최유권', '1', '0930', '제1투표장', 'Y');
+insert into tbl_vote_202005 values('59010110007', '지유권', '1', '0930', '제1투표장', 'Y');
+insert into tbl_vote_202005 values('49010120008', '장유권', '3', '0930', '제1투표장', 'Y');
+insert into tbl_vote_202005 values('79010110009', '정유권', '3', '0930', '제1투표장', 'Y');
+insert into tbl_vote_202005 values('89010120010', '강유권', '4', '0930', '제1투표장', 'Y');
+insert into tbl_vote_202005 values('99010110011', '신유권', '5', '0930', '제1투표장', 'Y');
+insert into tbl_vote_202005 values('79010120012', '오유권', '1', '1330', '제1투표장', 'Y');
+insert into tbl_vote_202005 values('69010110013', '현유권', '4', '1330', '제2투표장', 'Y');
+insert into tbl_vote_202005 values('89010110014', '왕유권', '2', '1330', '제2투표장', 'Y');
+insert into tbl_vote_202005 values('99010110015', '유유권', '3', '1330', '제2투표장', 'Y');
+insert into tbl_vote_202005 values('79010110016', '한유권', '2', '1330', '제2투표장', 'Y');
+insert into tbl_vote_202005 values('89010110017', '문유권', '4', '1330', '제2투표장', 'Y');
+insert into tbl_vote_202005 values('99010110018', '양유권', '2', '1330', '제2투표장', 'Y');
+insert into tbl_vote_202005 values('79010110019', '구유권', '4', '1330', '제2투표장', 'Y');
+insert into tbl_vote_202005 values('69010110020', '황유권', '5', '1330', '제2투표장', 'Y');
+insert into tbl_vote_202005 values('79010110021', '배유권', '3', '1330', '제2투표장', 'Y');
+insert into tbl_vote_202005 values('79010110022', '전유권', '3', '1330', '제2투표장', 'Y');
+insert into tbl_vote_202005 values('99010110023', '고유권', '1', '1330', '제2투표장', 'Y');
+insert into tbl_vote_202005 values('59010110024', '권유권', '3', '1330', '제2투표장', 'Y');
+
+select * from tbl_vote_202005;
+
+create table tbl_member_202005(
+	m_no char(1) primary key,
+	m_name varchar2(20),
+	p_code char(2),
+	p_school char(1),
+	m_jumin char(13),
+	m_city varchar(20)
+);
+
+insert into tbl_member_202005 values('1', '김후보', 'P1', '1', '660311999991', '수선화동');
+insert into tbl_member_202005 values('2', '이후보', 'P2', '3', '550311999992', '민들래동');
+insert into tbl_member_202005 values('3', '박후보', 'P3', '2', '770311999993', '나팔꽃동');
+insert into tbl_member_202005 values('4', '조후보', 'P4', '2', '880311999994', '진달래동');
+insert into tbl_member_202005 values('5', '최후보', 'P5', '3', '990311999995', '개나리동');
+
+select * from tbl_member_202005;
+
+create table tbl_party_202005 (
+            p_code char(2) primary key,
+            p_name varchar2(20),
+            p_indate date,
+            p_reader varchar2(20),
+            p_tel1 char(3),
+            p_tel2 char(4),
+            p_tel3 char(4)
+);
+
+insert into tbl_party_202005 values('P1', 'A정당', '2010-01-01', '위대표', '02', '1111', '0001');
+insert into tbl_party_202005 values('P2', 'B정당', '2010-02-01', '명대표', '02', '1111', '0002');
+insert into tbl_party_202005 values('P3', 'C정당', '2010-03-01', '기대표', '02', '1111', '0003');
+insert into tbl_party_202005 values('P4', 'D정당', '2010-04-01', '옥대표', '02', '1111', '0004');
+insert into tbl_party_202005 values('P5', 'E정당', '2010-05-01', '임대표', '02', '1111', '0005');
+
+select * from tbl_party_202005;
+select me.m_no, me.m_name, pa.p_name, 
+		case when me.p_school = '1' then '고졸'
+			when me.p_school = '2' then '학사'
+			when me.p_school = '3' then '석사'
+			else '박사' end as p_school 
+		, me.m_jumin, me.m_city, pa.p_tel1 || '-' || pa.p_tel2 || '-' || pa.p_tel3 as p_tel
+from TBL_VOTE_202005 vo, tbl_member_202005 me, tbl_party_202005 pa
+where me.m_no = vo.m_no and pa.p_code = me.p_code
+group by me.m_no, me.m_name, pa.p_name, me.p_school,me.m_jumin, me.m_city,pa.p_tel1, pa.p_tel2, pa.p_tel3;
